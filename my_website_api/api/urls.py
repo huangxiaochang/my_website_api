683ee47efa05db8b72f04c7ef12ebb9b38ef8c08
huangxiaochang <wb.huangxiaochang@mesg.corp.netease.com>
2018-06-04 14:05:22 +0800

    登录cookie

--- my_website_api/api/urls.py
+++ my_website_api/api/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'register/', views.RegisterView.as_view(), name="user_register")
+    # url(r'register/', views.RegisterView.as_view(), name="user_register")
+    url(r'user_info/', include('api.user_info.urls'))
 ]
